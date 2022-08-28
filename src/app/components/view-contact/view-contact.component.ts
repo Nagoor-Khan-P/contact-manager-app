2c29c6aaa027b6e214b11c22a51a964b00f22255
Nagoor-Khan-P <106923967+Nagoor-Khan-P@users.noreply.github.com>
2022-08-28 22:58:16 +0530

    view component completed with dynamic content by fettching data from parent

--- src/app/components/view-contact/view-contact.component.ts
+++ src/app/components/view-contact/view-contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { IContact } from 'src/app/models/IContact';
+import { IGroup } from 'src/app/models/IGroup';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-view-contact',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewContactComponent implements OnInit {
 
-  constructor() { }
+  private contactId : string | null = null;
+
+  public loading: boolean=false;
+  public contact:IContact;
+  public errorMessage : string | null;
+  public group:IGroup;
+
+  constructor(private activatedRoute : ActivatedRoute, private contactService : ContactService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((param : Params) => {
+      this.contactId=param.get('contactId'); //getting id fron the url
+
+      this.loading=true;
+      this.contactService.getContactById(this.contactId).subscribe((data : IContact) => {
+        this.contact=data;
+        this.loading=false;
+        this.contactService.getGroupById(data).subscribe((data) => {
+          this.group=data;
+        })
+      },(error) => {
+        this.errorMessage=error;
+        this.loading=false;
+      })
+    })
+  }
+
+  public isEmpty(){
+    return Object.keys(this.contact).length >0;
+  }
+
+  public isGroupEmpty(){
+    return Object.keys(this.group).length > 0;
   }
 
 }
