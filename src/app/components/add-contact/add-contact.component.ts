5c85cb72df74d992c61fee7de915a6035fa8b68d
Nagoor-Khan-P <106923967+Nagoor-Khan-P@users.noreply.github.com>
2022-08-28 23:57:32 +0530

    add component completed

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { IContact } from 'src/app/models/IContact';
+import { IGroup } from 'src/app/models/IGroup';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-add-contact',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddContactComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean=false;
+  public contact : IContact={} as IContact;
+  public errorMessage : string | null=null;
+  public groups: IGroup[] = [] as IGroup[];
+
+  constructor(private contactService : ContactService, private router: Router) { }
 
   ngOnInit() {
+    this.loading=true;
+    this.contactService.getAllGrops().subscribe((data) => {
+      this.groups=data;
+      this.loading=false;
+    },(error) => {
+      this.errorMessage=error;
+      this.loading=false;
+    })
+  }
+
+  public createSubmit(){
+    this.contactService.createContact(this.contact).subscribe((data) => {
+      this.router.navigate(['/']).then();
+    },(error) => {
+      this.errorMessage=error;
+      this.router.navigate(['/contacts/add']).then();
+    })
   }
 
 }
