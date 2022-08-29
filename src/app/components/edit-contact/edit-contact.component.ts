a6d62661dd3b4d53870b1b057ed280857939f389
Nagoor-Khan-P <106923967+Nagoor-Khan-P@users.noreply.github.com>
2022-08-29 22:39:52 +0530

    updating contact details component completed

--- src/app/components/edit-contact/edit-contact.component.ts
+++ src/app/components/edit-contact/edit-contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { IContact } from 'src/app/models/IContact';
+import { IGroup } from 'src/app/models/IGroup';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-edit-contact',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditContactComponent implements OnInit {
 
-  constructor() { }
+  public loading : boolean =false;
+  public contactId : string | null=null;
+  public contact: IContact ={} as IContact;
+  public errorMessage : string | null =null;
+  public groups : IGroup[] =[] as IGroup[];
+
+  constructor(private contactService : ContactService, private activatedRoute: ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((param : Params) => {
+      this.contactId=param.get('contactId'); //use ativatedRoute and parammap to get id from the url 
+
+      this.loading=true;
+      this.contactService.getContactById(this.contactId).subscribe((data) => {
+        this.contact=data;
+        this.loading=false;
+        this.contactService.getAllGrops().subscribe((data : IGroup[]) => {
+          this.groups=data;
+        })
+      },(error) => {
+        this.errorMessage=error;
+        this.loading=false;
+      })
+    })
+  }
+
+  public updateSubmit(){
+    this.contactService.updateContact(this.contact,this.contactId).subscribe((data) => {
+      this.router.navigate(['/']).then();
+    }, (error) => {
+      this.errorMessage=error;
+      this.router.navigate([`/contacts/edit/${this.contactId}`]).then();
+    })
   }
 
 }
