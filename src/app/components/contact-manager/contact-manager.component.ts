2df8f1e295815c81a75ec3349c18b9f238cbfdef
Nagoor-Khan-P <106923967+Nagoor-Khan-P@users.noreply.github.com>
2022-08-29 22:59:59 +0530

    added the delete contact functionality

--- src/app/components/contact-manager/contact-manager.component.ts
+++ src/app/components/contact-manager/contact-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IContact } from 'src/app/models/IContact';
 import { ContactService } from 'src/app/services/contact.service';
 
@@ -15,13 +16,28 @@ export class ContactManagerComponent implements OnInit {
 
 
 
-  constructor(private contactService : ContactService) { }
+  constructor(private contactService : ContactService, private router : Router) { }
 
   ngOnInit() {
+    this.getAllContactsFromServer();
+  }
+
+  public getAllContactsFromServer(){
     this.loading=true;
     this.contactService.getAllContacts().subscribe( (data : IContact[]) => {
       this.contacts=data;
       this.loading=false;//stopping the spinner
+    }, (error) => {
+      this.errorMessage=error;
+      this.loading=false;
+    })
+  }
+
+  public clickDeleteContact(contactId : string | undefined){
+    this.loading=true;
+    this.contactService.deleteContact(contactId).subscribe((data) => {
+      this.getAllContactsFromServer();
+      this.loading=false;
     }, (error) => {
       this.errorMessage=error;
       this.loading=false;
